29f27b5c9a61d374c8795581893f0a78ddf181c2
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-12-23 18:55:35 +0100

    Pass command-line args

--- src/main/scala/BranchAndBound.scala
+++ src/main/scala/BranchAndBound.scala
@@ -21,9 +21,15 @@ object InputData {
 	}.toArray
 
 	val deadline = 30
-	val length = 16
-	val execTimes = randomValues(length, deadline) // Array(3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25)
-	val taskCount = execTimes.length
+
+	var taskCount = 0
+	var execTimes: Array[Int] = null
+	var recursionMaxDepth = 1
+
+	def initExecTimes(n: Int) {
+		taskCount = n
+		execTimes = randomValues(taskCount, deadline)
+	}
 
 	def evaluate(consumedTime: Array[Int]) =
 		consumedTime.reduceLeft(_ max _) * consumedTime.filter(_ > 0).length
@@ -32,16 +38,14 @@ object InputData {
 class Worker extends Actor {
 	import InputData._
 
-	val recursionMaxDepth = 1
 	var localBestResult = taskCount * deadline
 
 	def solveTaskRecursive(index: Int, consumedTime: Array[Int], machinesBooted: Int, manager: ActorRef, rootIndex: Int) {
-		//println("solveTaskRecursive, index = " + rootIndex + " -> " + index + ", consumedTime = " + consumedTime.mkString(" "))
 		if (index == taskCount) {
 			val result = evaluate(consumedTime)
 			if (result < localBestResult) {
 				localBestResult = result
-				println(consumedTime.mkString(" ") + "; " + result)
+				//println(consumedTime.filter(_ > 0).mkString(" ") + "; " + result)
 			}
 		} else if (index == rootIndex + recursionMaxDepth) {
 			manager ! Task(index, consumedTime, machinesBooted)
@@ -119,12 +123,14 @@ class Overlord extends Actor {
 	def receive = {
 		case result: Int =>
 			var elapsed = System.nanoTime - start
-			println("Parallel result: " + result + ", elapsed time: " + elapsed / 1e6 + " millisec")
+			//println("Parallel result: " + result + ", elapsed time: " + elapsed / 1e6 + " millisec")
+			println(elapsed / 1e6)
 			context.system.shutdown()
 			start = System.nanoTime
 			Sequential.run(0, new Array[Int](taskCount), 0)
 			elapsed = System.nanoTime - start
-			println("Sequential result: " + Sequential.bestResult + ", elapsed time: " + elapsed / 1e6 + " millisec")
+			//println("Sequential result: " + Sequential.bestResult + ", elapsed time: " + elapsed / 1e6 + " millisec")
+			println(elapsed / 1e6)
 	}
 }
 
@@ -137,7 +143,7 @@ object Sequential {
 			val result = evaluate(consumedTime)
 			if (result < bestResult) {
 				bestResult = result
-				println(consumedTime.mkString(" ") + "; " + result)
+				//println(consumedTime.filter(_ > 0).mkString(" ") + "; " + result)
 			}
 		} else {
 			val time = execTimes(index)
@@ -155,6 +161,8 @@ object Sequential {
 
 object Main {
 	def main(args: Array[String]) {
+		InputData.initExecTimes(args(0).toInt)
+		InputData.recursionMaxDepth = args(1).toInt
 		ActorSystem("BranchAndBound").actorOf(Props(classOf[Overlord]), "overlord")		
 	}
 }
